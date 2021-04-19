bbb3577b099ad9a71a09db6c6f5c92a34b94b7c9
Davide Della Casa <davidedc@gmail.com>
2021-04-19 19:41:03 +0100

    all info about a version is now declared in a .js

--- whats-the-latest-version.js
+++ whats-the-latest-version.js
@@ -1,1 +1,112 @@
-latestStableAlgebriteVersion = "1.4.0";
+var latestStableAlgebriteVersion = "1.4.0";
+
+var versionsData = {
+    "1.4.0": {
+        "changes": "asasas",
+        "improved": "asasas",
+        "other info": "asasas",
+        "known issues": "asasas",
+        "other links": {
+            "link 1": {
+                "title": "why the tensor notation changes in v1.0.0?",
+                "url": "why-tensor-notation-changes-in-v1.0.0.html"
+            }
+        }
+    },
+    "1.3.1": {
+        "changes": "asasas",
+        "improved": "asasas",
+        "other info": "asasas",
+        "known issues": "asasas",
+        "other links": {
+            "link 1": {
+                "title": "why the tensor notation changes in v1.0.0?",
+                "url": "why-tensor-notation-changes-in-v1.0.0.html"
+            }
+        }
+    },
+    "1.3.0": {
+        "changes": "asasas",
+        "improved": "asasas",
+        "other info": "asasas",
+        "known issues": "asasas",
+        "other links": {
+            "link 1": {
+                "title": "why the tensor notation changes in v1.0.0?",
+                "url": "why-tensor-notation-changes-in-v1.0.0.html"
+            }
+        }
+    },
+    "1.2.1": {
+        "changes": "asasas",
+        "improved": "asasas",
+        "other info": "asasas",
+        "known issues": "asasas",
+        "other links": {
+            "link 1": {
+                "title": "why the tensor notation changes in v1.0.0?",
+                "url": "why-tensor-notation-changes-in-v1.0.0.html"
+            }
+        }
+    },
+    "1.2.0": {
+        "changes": "asasas",
+        "improved": "asasas",
+        "other info": "asasas",
+        "known issues": "asasas",
+        "other links": {
+            "link 1": {
+                "title": "why the tensor notation changes in v1.0.0?",
+                "url": "why-tensor-notation-changes-in-v1.0.0.html"
+            }
+        }
+    },
+    "1.0.0": {
+        "changes": "asasas",
+        "improved": "asasas",
+        "other info": "asasas",
+        "known issues": "asasas",
+        "other links": {
+            "link 1": {
+                "title": "why the tensor notation changes in v1.0.0?",
+                "url": "why-tensor-notation-changes-in-v1.0.0.html"
+            }
+        }
+    },
+    "0.4.2": {
+        "changes": "asasas",
+        "improved": "asasas",
+        "other info": "asasas",
+        "known issues": "asasas",
+        "other links": {
+            "link 1": {
+                "title": "why the tensor notation changes in v1.0.0?",
+                "url": "why-tensor-notation-changes-in-v1.0.0.html"
+            }
+        }
+    },
+    "0.3.13": {
+        "changes": "asasas",
+        "improved": "asasas",
+        "other info": "asasas",
+        "known issues": "asasas",
+        "other links": {
+            "link 1": {
+                "title": "why the tensor notation changes in v1.0.0?",
+                "url": "why-tensor-notation-changes-in-v1.0.0.html"
+            }
+        }
+    },
+    "0.2.23": {
+        "changes": "asasas",
+        "improved": "asasas",
+        "other info": "asasas",
+        "known issues": "asasas",
+        "other links": {
+            "link 1": {
+                "title": "why the tensor notation changes in v1.0.0?",
+                "url": "why-tensor-notation-changes-in-v1.0.0.html"
+            }
+        }
+    }
+};
