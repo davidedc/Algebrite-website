89624f96bdf6f4ff24340df6c527168799a63261
Davide Della Casa <davidedc@gmail.com>
2021-04-21 21:31:58 +0100

    filling-in more info on the releases (via javascript)

--- whats-the-latest-version.js
+++ whats-the-latest-version.js
@@ -9,111 +9,104 @@ var versionsData = {
 "More functions now using JS booleans rather than the C-style integers '0' and '1'. " +
 "Fixed #121 '1 - 1/2*0^2 results in divide by zero error'. Refactoring.",
 
-        "improved": "asasas",
-        "other info": "asasas",
-        "known issues": "asasas",
-        "other links": {
-            "link 1": {
-                "title": "why the tensor notation changes in v1.0.0?",
-                "url": "why-tensor-notation-changes-in-v1.0.0.html"
-            }
-        }
+        "improved": null,
+        "other info": null,
+        "known issues": null,
+        "other links": null
     },
     "1.3.1": {
-        "changes": "asasas",
-        "improved": "asasas",
-        "other info": "asasas",
-        "known issues": "asasas",
-        "other links": {
-            "link 1": {
-                "title": "why the tensor notation changes in v1.0.0?",
-                "url": "why-tensor-notation-changes-in-v1.0.0.html"
-            }
-        }
+        "changes":
+
+"Fixing some problems with zero matrices.",
+
+        "improved": null,
+        "other info": null,
+        "known issues": null,
+        "other links": null
     },
     "1.3.0": {
-        "changes": "asasas",
-        "improved": "asasas",
-        "other info": "asasas",
-        "known issues": "asasas",
-        "other links": {
-            "link 1": {
-                "title": "why the tensor notation changes in v1.0.0?",
-                "url": "why-tensor-notation-changes-in-v1.0.0.html"
-            }
-        }
+        "changes":
+
+"Implemented hashing to speed up the integral table. Changes to erf/erfc so they handle symbolic " +
+"and numeric evaluation at one/zero better. Better printouts (and options to printout) doubles: " +
+"we can now set forceFixedPrintout (defaults to true) and maxFixedPrintoutDigits (defaults to 6) " +
+"- closes #85. Refactoring how polys detection works (can now detect polys in factored form), " +
+"refactoring gcd and adding gcd of polys (via factoring though). Can now detect and simplify many " +
+"rational expressions. Closes #89.",
+
+        "improved": null,
+        "other info": null,
+        "known issues": null,
+        "other links": null
     },
     "1.2.1": {
-        "changes": "asasas",
-        "improved": "asasas",
-        "other info": "asasas",
-        "known issues": "asasas",
-        "other links": {
-            "link 1": {
-                "title": "why the tensor notation changes in v1.0.0?",
-                "url": "why-tensor-notation-changes-in-v1.0.0.html"
-            }
-        }
+        "changes":
+
+"changing tabs into spaces so it works with CoffeeScript 2. Where native BigInt is implemented, " +
+"use a dedicated isSmall implementation - closes #78.",
+
+        "improved": null,
+        "other info": null,
+        "known issues": null,
+        "other links": null
     },
     "1.2.0": {
-        "changes": "asasas",
-        "improved": "asasas",
-        "other info": "asasas",
-        "known issues": "asasas",
-        "other links": {
-            "link 1": {
-                "title": "why the tensor notation changes in v1.0.0?",
-                "url": "why-tensor-notation-changes-in-v1.0.0.html"
-            }
-        }
+        "changes":
+
+"results are now printed in 'computer' notation by default / several bug fixes / 'for', 'sum', " +
+"'product' arguments and their order have changed (as per new reference docs) / error returned " +
+"when something other than a function is used in function invocation / 'printfull' -> 'printcomputer' " +
+" / 'printplain' -> 'printhuman'",
+
+        "improved": null,
+        "other info": null,
+        "known issues": null,
+        "other links": null
     },
     "1.0.0": {
-        "changes": "asasas",
-        "improved": "asasas",
-        "other info": "asasas",
-        "known issues": "asasas",
-        "other links": {
-            "link 1": {
-                "title": "why the tensor notation changes in v1.0.0?",
-                "url": "why-tensor-notation-changes-in-v1.0.0.html"
-            }
-        }
+        "changes":
+
+"code generation + latex generation for matrices / can now pass functions as arguments / overhaul " +
+"of the matrix notation (see dedicated document in links above) / some refactoring",
+
+        "improved": null,
+        "other info": null,
+        "known issues": null,
+        "other links": null
     },
     "0.4.2": {
-        "changes": "asasas",
-        "improved": "asasas",
-        "other info": "asasas",
-        "known issues": "asasas",
-        "other links": {
-            "link 1": {
-                "title": "why the tensor notation changes in v1.0.0?",
-                "url": "why-tensor-notation-changes-in-v1.0.0.html"
-            }
-        }
+        "changes": null,
+        "improved":
+
+"fixed all issues found in previous version. Unified 'mag' into 'abs' (just use abs all the times). " +
+"sqrt(x^2) is now correct and returns abs(x). All doubles now show the decimal point when printed. " +
+"abs, imag, arg, rect, legendre are now in general more correct than in the previous version.",
+
+        "other info": "Adding a flag 'assumeRealVariables' for assuming/not assuming variables to be real.",
+        "known issues": "4 integrals from previous version aren't solved anymore.",
+        "other links": null
     },
     "0.3.13": {
-        "changes": "asasas",
-        "improved": "asasas",
-        "other info": "asasas",
-        "known issues": "asasas",
-        "other links": {
-            "link 1": {
-                "title": "why the tensor notation changes in v1.0.0?",
-                "url": "why-tensor-notation-changes-in-v1.0.0.html"
-            }
-        }
+        "changes": null,
+        "improved":
+
+"added 'version', 'clear', 'clearall', refactored 'print' and added all the new print* functions. ",
+
+        "other info": "Adding a flag 'assumeRealVariables' for assuming/not assuming variables to be real.",
+        "known issues":
+
+"'float(sin(2))' gives zero. 'sqrt(x^2)' gives x instead of abs(x). Incorrect " +
+"simplifications for: sqrt(x*y) , sqrt(1/x) , sqrt(x^y). Latex printing: wrong formatting of exponents " +
+"that are more than one character long.",
+
+        "other links": null
     },
     "0.2.23": {
-        "changes": "asasas",
-        "improved": "asasas",
-        "other info": "asasas",
-        "known issues": "asasas",
-        "other links": {
-            "link 1": {
-                "title": "why the tensor notation changes in v1.0.0?",
-                "url": "why-tensor-notation-changes-in-v1.0.0.html"
-            }
-        }
+        "changes": "One of the first releases.",
+        "improved": null,
+        "other info": null,
+        "known issues": null,
+        "other links": null
     }
 };
 
