b667bef56a9b3418211c48bc2d7f28b3b97b804d
Davide Della Casa <davidedc@gmail.com>
2021-04-25 21:42:18 +0100

    more of the docs page being generated via the versions data in the js file

--- versions-data-utils.js
+++ versions-data-utils.js
@@ -1,5 +1,6 @@
 var theVersions = Object.keys(versionsData);
 // sorting semver routine from https://stackoverflow.com/a/40201629
 var sortedVersions = theVersions.map( a => a.split('.').map( n => +n+100000 ).join('.') ).sort().map( a => a.split('.').map( n => +n-100000 ).join('.') );
+var sortedVersionsFromNewToOld = sortedVersions.slice().reverse();
 
 var latestStableAlgebriteVersion = sortedVersions[sortedVersions.length-1];
